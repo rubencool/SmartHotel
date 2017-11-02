e2df533029c8398568cddab07b519be55299f4de
Bishnu <sbishnu019@gmail.com>
2017-11-02 15:17:01 +0545

    auto register when Qrcode is Scaned

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,16 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import Quagga from 'quagga'
-// import jsqrcode from 'jsqrcode'
 
 require('../node_modules/material-design-lite/material.min.css')
 require('../node_modules/material-design-lite/material.min.js')
-require('../node_modules/quagga/dist/quagga.min.js')
-
 Vue.config.productionTip = false
-
-// const Quagga = require('quagga').default
 
 /* eslint-disable no-new */
 new Vue({
